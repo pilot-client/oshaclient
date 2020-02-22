0c1c9b41fc7ae58c0287ecd140102ae396581a2d
zentpvp <60325136+zentpvp@users.noreply.github.com>
2020-02-22 12:48:37 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
     connection: {
-        host: '000webhost.com',
+        host: 'databases.000webhost.com',
         user: 'id12676389_zentclient',
         password: 'zentclientzentclientzentclient',
     },
