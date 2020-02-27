b865e19e3eee54c32a561e46277be56661959b59
zentpvp <60325136+zentpvp@users.noreply.github.com>
2020-02-26 23:01:11 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     connection: {
         host: 'remotemysql.com',
         user: 'ALmAD2mVET',
-        password: X6onhH0mRv',
+        password: X6onhH0mRv,
     },
     database: 'PALmAD2mVET'
 };
